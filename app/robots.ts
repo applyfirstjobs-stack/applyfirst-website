bfa5ee9c1d033a5ddc0153f86269d6c37de81998
applyfirstjobs-stack <applyfirstjobs@gmail.com>
2026-04-17 21:24:09 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,19 @@
-
+import type { MetadataRoute } from 'next';
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: [
+      'https://www.applyfirstjobs.com/sitemap.xml',
+      'https://www.applyfirstjobs.com/sitemap/1.xml',
+      'https://www.applyfirstjobs.com/sitemap/2.xml',
+      'https://www.applyfirstjobs.com/sitemap/3.xml',
+      'https://www.applyfirstjobs.com/sitemap/4.xml',
+      'https://www.applyfirstjobs.com/sitemap/5.xml',
+    ],
+  };
+}
+ 
